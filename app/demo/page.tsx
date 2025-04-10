565efecb2f5cecb7a83db3d0e0246996b7e03622
ayush <v-ayushg@microsoft.com>
2025-04-10 23:45:22 +0000

    forces runtime ssr

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,4 +1,7 @@
-export default async function demo() {
-    console.log("This is demo of server side logs......................");
-    return <h1>This is H1 of demo for server side logs</h1>;
+// app/demo/page.tsx (forces runtime SSR)
+export const dynamic = "force-dynamic"; // tells Next.js to NOT statically pre-render
+
+export default async function Demo() {
+    console.log("Runtime server-side log of demo page!..................................");
+    return <h1>This is Demo Page</h1>;
 }

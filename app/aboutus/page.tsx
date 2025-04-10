574352da622f13360e9f45f3228fa29a1241f572
ayush <v-ayushg@microsoft.com>
2025-04-10 21:23:03 +0000

    fix elint

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 export default function AboutUsPage() {
-    console.log("this is /about  client CONSOLE..................")
-  return <h1>About Us - Welcome to Ayush Gupta's site!</h1>;
+    console.log("this is about  client CONSOLE..................")
+  return <h1>About Us - Welcome to Ayush Gupta site!</h1>;
 }
